aa27e3f029c1e4ab13bef186118f411bdda7fdb7
Savelii Kosevich <savva0512@gmail.com>
2024-08-14 20:33:39 +0300

    added ability to choose units

--- main.js
+++ main.js
@@ -27,70 +27,6 @@ async function userLocation() {
 
 let local = "";
 
-const Apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
-let myApiKey = "2648abbb783df953999876283f68f540";
-const cityInput = document.querySelector(".city__input");
-
-async function getData(city) {
-  try {
-    const resp = await fetch(Apiurl + city + `&appid=${myApiKey}`, { mode: "cors" });
-    const data = await resp.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log("Error fetching weather data:", error);
-  }
-}
-
-async function makeCard(city) {
-  console.log(city);
-  let data = await getData(city);
-
-  if (data.cod != 200) {
-    document.querySelector(".city").innerHTML = data?.name || "N/A";
-    document.querySelector(".temp").innerHTML = Math.round(data.main?.temp) || "N/A";
-    document.querySelector(".weather-main").innerHTML = data.weather?.[0]?.main || "N/A";
-    document.querySelector(".country").innerHTML = data.sys?.country || "N/A";
-    document.querySelector("#wind").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
-    document.querySelector("#rainFall").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
-    document.querySelector("#humidity").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
-    document.querySelector(".temp__img").classList.add("hidden");
-  } else {
-    document.querySelector(".temp__img").classList.remove("hidden");
-    document.querySelector(".city").innerHTML = data?.name || "N/A";
-    document.querySelector(".temp").innerHTML = Math.round(data.main?.temp) || "N/A";
-    document.querySelector(".weather-main").innerHTML = data.weather?.[0]?.main || "N/A";
-    document.querySelector(".country").innerHTML = data.sys?.country || "N/A";
-    document.querySelector("#wind").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
-  }
-
-  if (!city) {
-    document.querySelector(".wether__img").src = "unload.png";
-  } else if (data.weather[0].main == "Thunderstorm") {
-    document.querySelector(".wether__img").src = "TStorm.png";
-  } else if (data.weather[0].main == "Drizzle") {
-    document.querySelector(".wether__img").src = "drizle.png";
-  } else if (data.weather[0].main == "Rain") {
-    document.querySelector(".wether__img").src = "rain.png";
-  } else if (data.weather[0].main == "Snow") {
-    document.querySelector(".wether__img").src = "snow.png";
-  } else if (data.weather[0].main == "Clouds") {
-    document.querySelector(".wether__img").src = "cloudy.png";
-  } else if (data.weather[0].main == "Clear") {
-    document.querySelector(".wether__img").src = "clear.png";
-  }
-}
-
-document.addEventListener("DOMContentLoaded", async () => {
-  const locationCity = await userLocation();
-  makeCard(locationCity);
-});
-
-cityInput.addEventListener("change", () => {
-  makeCard(cityInput.value);
-  console.log("Selected city:", cityInput.value);
-});
-
 const timeApi = "http://worldtimeapi.org/api/timezone/Europe/Vaduz";
 
 async function timeZone() {
@@ -114,3 +50,102 @@ async function timeZone() {
 }
 
 timeZone();
+
+const Apiurl = `https://api.openweathermap.org/data/2.5/weather?`;
+let myApiKey = "2648abbb783df953999876283f68f540";
+const cityInput = document.querySelector(".city__input");
+
+async function getData(city, unit) {
+  try {
+    const resp = await fetch(Apiurl + `units=${unit}&q=` + city + `&appid=${myApiKey}`, {
+      mode: "cors",
+    });
+    const data = await resp.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("Error fetching weather data:", error);
+  }
+}
+
+async function makeCard(city, unit) {
+  console.log(city);
+  const tempUnit = document.querySelector(".temp__img");
+  unit === "metric" ? (tempUnit.innerHTML = "°C") : (tempUnit.innerHTML = "°F");
+  let data = await getData(city, unit);
+
+  if (data.cod != 200) {
+    document.querySelector(".city").innerHTML = data?.name || "N/A";
+    document.querySelector(".temp").innerHTML = Math.round(data.main?.temp) || "N/A";
+    document.querySelector(".weather-main").innerHTML = data.weather?.[0]?.main || "N/A";
+    document.querySelector(".country").innerHTML = data.sys?.country || "N/A";
+    document.querySelector("#wind").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
+    document.querySelector("#rainFall").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
+    document.querySelector("#humidity").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
+    document.querySelector(".temp__img").classList.add("hidden");
+  } else {
+    document.querySelector(".temp__img").classList.remove("hidden");
+    document.querySelector(".city").innerHTML = data?.name || "N/A";
+    document.querySelector(".temp").innerHTML = Math.round(data.main?.temp) || "N/A";
+    document.querySelector(".weather-main").innerHTML = data.weather?.[0]?.main || "N/A";
+    document.querySelector(".country").innerHTML = data.sys?.country || "N/A";
+    document.querySelector("#wind").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
+    document.querySelector("#humidity").innerHTML = Math.round(data.main?.humidity) + " %" || "N/A";
+    // document.querySelector("#rainFall").innerHTML = Math.round(data.wind?.speed) + " km/h" || "N/A";
+  }
+
+  if (!city) {
+    document.querySelector(".wether__img").src = "unload.png";
+  } else if (data.weather[0].main == "Thunderstorm") {
+    document.querySelector(".wether__img").src = "TStorm.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    document.querySelector(".wether__img").src = "drizle.png";
+  } else if (data.weather[0].main == "Rain") {
+    document.querySelector(".wether__img").src = "rain.png";
+  } else if (data.weather[0].main == "Snow") {
+    document.querySelector(".wether__img").src = "snow.png";
+  } else if (data.weather[0].main == "Clouds") {
+    document.querySelector(".wether__img").src = "cloudy.png";
+  } else if (data.weather[0].main == "Clear") {
+    document.querySelector(".wether__img").src = "clear.png";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  let unit = units.checked ? " imperial" : "metric";
+  const locationCity = await userLocation();
+  makeCard(locationCity, unit);
+});
+
+cityInput.addEventListener("change", () => {
+  let unit = units.checked ? " imperial" : "metric";
+  console.log(unit);
+  makeCard(cityInput.value, unit);
+  console.log("Selected city:", cityInput.value);
+});
+
+const ApiHourly = `https://api.openweathermap.org/data/2.5/forecast?`;
+
+async function getDataHourly(lat, lon) {
+  try {
+    const resp = await fetch(ApiHourly + `lat=${lat}&lon=${lon}` + `&appid=${myApiKey}`, {
+      mode: "cors",
+    });
+    const data = await resp.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("Error fetching weather data:", error);
+  }
+}
+
+getDataHourly(53.9, 27.5667);
+
+const units = document.querySelector(".unit");
+
+units.addEventListener("click", () => {
+  let unit = units.checked ? " imperial" : "metric";
+
+  console.log(unit);
+  makeCard(cityInput.value, unit);
+});
